850c2f257fd11840de10afe35cb6796806084717
Alexandre Assouad <alexandre.assouad@gmail.com>
2014-04-08 14:20:53 +0200

    removed copyright

--- src/Krizon/Google/Analytics/MeasurementProtocol/Resources/service.php
+++ src/Krizon/Google/Analytics/MeasurementProtocol/Resources/service.php
@@ -4,7 +4,6 @@
  * This file is part of the php-ga-measurement-protocol package.
  *
  * (c) Kristian Zondervan <kristian.zondervan@gmail.com>
- * (c) Alexandre Assouad <alexandre.assouad@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
